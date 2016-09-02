8d360aeb129ad3a92c09fde9e6e41a4e717fa1e1
attila <attila>
2016-09-02 23:52:14 +0200

    TimeStampedModel

--- config/settings/local.py
+++ config/settings/local.py
@@ -5,3 +5,16 @@ SECRET_KEY = '^j=omww9y21hh&v8mtm*jz2uyfn^zpfc436yj_aebtcf8g@1%+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
+# Database
+# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'testdb',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
--- config/settings/production.py
+++ config/settings/production.py
@@ -6,6 +6,19 @@ SECRET_KEY = get_env_variable("YANNI_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+# Database
+# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': get_env_variable("YANNI_DB_NAME"),
+        'USER': get_env_variable("YANNI_DB_USER"),
+        'PASSWORD': get_env_variable("YANNI_DB_PASSWORD"),
+        'HOST': get_env_variable("YANNI_DB_HOST"),
+        'PORT': get_env_variable("YANNI_DB_PORT"),
+    }
+}
+
 reminder_for_myself = """
     Check out base.py and https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/ before going to production
     """
