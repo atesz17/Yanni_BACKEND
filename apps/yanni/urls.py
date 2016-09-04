28d7564e71a37e0e931f82b36c8496b944f51a19
attila <attila>
2016-09-04 22:13:49 +0200

    url namespace

--- apps/yanni/urls.py
+++ apps/yanni/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 
 from .views import CuttingWheelList
 
+app_name = "yanni"
 urlpatterns = [
-    url(r'^$', CuttingWheelList.as_view())
+    url(r'^$', CuttingWheelList.as_view(), name='cutting-wheel-list')
 ]
