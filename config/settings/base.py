66ca0359b06cc3aaf91a59b2fda6513ee56ddca6
attila <attila>
2016-09-05 21:28:41 +0200

    boostrap

--- config/settings/base.py
+++ config/settings/base.py
@@ -115,3 +115,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(PROJECT_ROOT_PATH, "vendor"), # 3rd party libraries
+    os.path.join(PROJECT_ROOT_PATH, "static")  # site-wide custom static files
+]
